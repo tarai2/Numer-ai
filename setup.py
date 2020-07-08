1432ef31aac19b32d35e1aad468494f0bcfedaf6
at0137 <arai.t.ar@m.titech.ac.jp>
2020-07-08 20:11:22 +0900

    firstcommit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="Numer-ai",
+    version="0.0.1",
+    install_requires = 
+    [
+        "numpy",
+        "pandas",
+        "tensorflow",
+        "numba"
+    ]
+    
+    # extras_require =
+    # {
+    #     "develop": ["dev-packageA", "dev-packageB"]
+    # },
+
+    # entry_points =
+    # {
+    #     "console_scripts" :
+    #     [
+    #         "foo = package_name.module_name:func_name",
+    #         "foo_dev = package_name.module_name:func_name [develop]"
+    #     ]
+    # }
+)
